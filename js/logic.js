0e687b5925be7e3a293fcbd4a90179ed063e2f6b
Patricia Campos <patricia@krint.io>
2019-04-02 15:58:17 +1100

    Tic Tac Toe

--- js/logic.js
+++ js/logic.js
@@ -75,6 +75,8 @@ console.log(game.makeMove('x', 4))
 console.log(game.makeMove('o', 2)) //no board piece has been taken
 console.log(game.makeMove('x', 7)) //winner. you should not be able to make anymore moves */
 
+/////////////////////////////////////////////////////////////////////////////////////////////
+
 //read the JS just when the document is ready
 $(document).ready(function() {
 
@@ -110,21 +112,23 @@ const ticTacToe = {
   //check if the player is winner or if hapen a draw
   // function will receive a player
   checkForWin: function(player) {
+    //variable with a counter to be compared if the player is a winner
     let counter = 0;
+    //look all possibles winning combos
     for (let i = 0; i < winningCombos.length; i += 1) {
-      console.log(winningCombos[i]);
+
+      for (let j = 0; j < winningCombos[i].length; j += 1) {
+
+        for (let w = 0; w < player.length; w += 1 ) {
+          //if the counter of the player have 3 matches he is a winner
+          // if turnPlayers.get is 0 the winner is x other wise (else if) o is winner
+          if (counter === 3 && this.turnPlayers.get())
+        }
+
+      }
     }
   },
 
 }
 });
 
-let winningCombos = [
-  [1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]
-];
-for (let i = 0; i < winningCombos.length; i += 1) {
-  console.log(winningCombos[i]);
-  for (let j = 0; j < winningCombos[i].length; j += 1) {
-    console.log(winningCombos[j]);
-  }
-};
